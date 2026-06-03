f552ea841ecaad5a975aa6cd701a6f6d70856414
ebholi <uliver@gmx.ch>
2026-06-03 15:12:44 +0200

    Tank Zombie Class

---
+++ src/main/java/com/example/TankApocalypseZombie.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.zombie.Zombie;
+import net.minecraft.world.level.Level;
+
+public class TankApocalypseZombie extends BaseApocalypseZombie{
+    public TankApocalypseZombie(EntityType<? extends Zombie> entityType, Level world) {
+        super(entityType, world);
+    }
+
+    // Special Attributes
+}
