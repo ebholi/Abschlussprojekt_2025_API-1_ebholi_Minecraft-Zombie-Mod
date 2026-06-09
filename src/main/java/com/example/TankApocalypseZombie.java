2c2d12cd97e59469432124b718acdcd47f94811d
ebholi <uliver@gmx.ch>
2026-06-09 09:10:41 +0200

    Buffed Tank Knockback

--- src/main/java/com/example/TankApocalypseZombie.java
+++ src/main/java/com/example/TankApocalypseZombie.java
@@ -21,7 +21,7 @@ public class TankApocalypseZombie extends BaseApocalypseZombie{
                 .add(Attributes.KNOCKBACK_RESISTANCE, 0.3)
                 .add(Attributes.MOVEMENT_SPEED, 0.18)
                 .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, 0.3)
-                .add(Attributes.ATTACK_KNOCKBACK, 1.5)
+                .add(Attributes.ATTACK_KNOCKBACK, 2.5)
                 .add(Attributes.ATTACK_DAMAGE, 7)
                 .add(Attributes.STEP_HEIGHT, 1.125)
                 .add(Attributes.SAFE_FALL_DISTANCE, 5)
