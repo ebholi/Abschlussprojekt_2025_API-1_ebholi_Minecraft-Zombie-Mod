bf8c6385925f47ff6c733b39b6fc75f7ba1b89ce
ebholi <uliver@gmx.ch>
2026-06-16 10:37:12 +0200

    Added Comments

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,6 +1,5 @@
 package com.example.client;
 
-import com.example.ExampleMod;
 import com.example.ModEntityTypes;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
@@ -11,11 +10,15 @@ public class ExampleModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+        // Registering custom Zombies
+        // Vanilla Renderer for Base Zombie
         EntityRendererRegistry.register(
                 ModEntityTypes.BASE_APOCALYPSE_ZOMBIE_ENTITY_TYPE,
                 ZombieRenderer::new
         );
 
+        // GeckoLib Renderer Syntax
         EntityRendererRegistry.register(
                 ModEntityTypes.TANK_APOCALYPSE_ZOMBIE_ENTITY_TYPE,
                 context -> new GeoEntityRenderer<>(context, ModEntityTypes.TANK_APOCALYPSE_ZOMBIE_ENTITY_TYPE)
