f975ee059dba7497a2f7904479a0a1b7336ce3f6
ebholi <oliver.ebhardt@lernende.bbw.ch>
2026-06-23 15:07:43 +0200

    Clean Code

--- src/client/java/com/y_apocalypse_zombies/client/ModClient.java
+++ src/client/java/com/y_apocalypse_zombies/client/ModClient.java
@@ -6,12 +6,11 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
+// Client Side Logic
 public class ModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
-
-        // Registering custom Zombies
+        // Registering custom Zombies with the Client
         // Vanilla Renderer for Base Zombie
         EntityRendererRegistry.register(
                 ModEntityTypes.BASE_APOCALYPSE_ZOMBIE_ENTITY_TYPE,
