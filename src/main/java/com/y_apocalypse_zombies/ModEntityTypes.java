60bd8959a398d2a3641193cc32bcb4ab26523f06
ebholi <oliver.ebhardt@lernende.bbw.ch>
2026-06-23 14:32:33 +0200

    Clean Code

--- src/main/java/com/y_apocalypse_zombies/ModEntityTypes.java
+++ src/main/java/com/y_apocalypse_zombies/ModEntityTypes.java
@@ -10,25 +10,31 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 
+// Defines the Builders for the Entities from the Mod
+// A Builder is used whenever a new Entity is summoned manually or naturally
 public class ModEntityTypes {
+    // Builder for the Base Zombie
     public static final EntityType<BaseApocalypseZombie> BASE_APOCALYPSE_ZOMBIE_ENTITY_TYPE = register(
             "base_apocalypse_zombie",
             EntityType.Builder.<BaseApocalypseZombie>of(BaseApocalypseZombie::new, MobCategory.MONSTER)
-                    .sized(0.6f, 1.95f) // Default: 0.6f | 1.95f
+                    .sized(0.6f, 1.95f) // Hitbox Size
     );
 
+    // Builder for the Tank
     public static final EntityType<TankApocalypseZombie> TANK_APOCALYPSE_ZOMBIE_ENTITY_TYPE = register(
             "tank_apocalypse_zombie",
             EntityType.Builder.<TankApocalypseZombie>of(TankApocalypseZombie::new, MobCategory.MONSTER)
                     .sized(0.8f, 2.5f)
     );
 
+    // Builder for the Tank
     public static final EntityType<RusherApocalypseZombie> RUSHER_APOCALYPSE_ZOMBIE_ENTITY_TYPE = register(
             "rusher_apocalypse_zombie",
             EntityType.Builder.<RusherApocalypseZombie>of(RusherApocalypseZombie::new, MobCategory.MONSTER)
                     .sized(0.6f, 1.7f)
     );
 
+    // Creates a ResourceKey and adds it to Minecraft's Registry to allow the Zombies to appear in-game.
     private static <T extends Entity> EntityType<T> register(String name, EntityType.Builder<T> builder) {
         ResourceKey<EntityType<?>> key = ResourceKey.create(Registries.ENTITY_TYPE, Identifier.fromNamespaceAndPath(YApocalypseZombies.MOD_ID, name));
         return Registry.register(BuiltInRegistries.ENTITY_TYPE, key, builder.build(key));
@@ -38,6 +44,7 @@ public class ModEntityTypes {
         YApocalypseZombies.LOGGER.info("Registering EntityTypes for " + YApocalypseZombies.MOD_ID);
     }
 
+    // Registering the Entities' Attributes
     public static void registerYZombieAttributes() {
         FabricDefaultAttributeRegistry.register(BASE_APOCALYPSE_ZOMBIE_ENTITY_TYPE, BaseApocalypseZombie.createAttributes());
         FabricDefaultAttributeRegistry.register(TANK_APOCALYPSE_ZOMBIE_ENTITY_TYPE, TankApocalypseZombie.createAttributes());
