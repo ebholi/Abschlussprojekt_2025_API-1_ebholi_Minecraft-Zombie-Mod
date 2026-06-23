6352e5d4f5687e8e28dffd858d853c723614b72c
ebholi <oliver.ebhardt@lernende.bbw.ch>
2026-06-23 13:15:58 +0200

    Deleted Mixins & Refactored ExampleModClient to have a more professional name. TOOK SO LONG TO DO THIS AND IT WAS JUST BECAUSE OF fabric.mod.json

--- src/client/java/com/y_apocalypse_zombies/client/ModClient.java
+++ src/client/java/com/y_apocalypse_zombies/client/ModClient.java
@@ -1,4 +1,4 @@
-package com.example.client;
+package com.y_apocalypse_zombies.client;
 
 import com.example.ModEntityTypes;
 import net.fabricmc.api.ClientModInitializer;
@@ -6,7 +6,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
