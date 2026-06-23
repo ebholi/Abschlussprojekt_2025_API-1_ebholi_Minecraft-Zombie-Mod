a404a568985c5c7c082552003fd17f1f6ea5cf36
ebholi <oliver.ebhardt@lernende.bbw.ch>
2026-06-23 14:16:43 +0200

    Clean Code

--- src/main/java/com/y_apocalypse_zombies/YApocalypseZombies.java
+++ src/main/java/com/y_apocalypse_zombies/YApocalypseZombies.java
@@ -7,25 +7,16 @@ import org.slf4j.LoggerFactory;
 
 public class YApocalypseZombies implements ModInitializer {
     public static final String MOD_ID = "y_apocalypse_zombies";
-
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
         LOGGER.info("Initializing y_apocalypse_zombies Entity Types");
         ModEntityTypes.registerModEntityTypes();
         LOGGER.info("Initializing y_apocalypse_zombies Attributes");
         ModEntityTypes.registerYZombieAttributes();
         LOGGER.info("Initializing y_apocalypse_zombies Spawn Spawn Rules");
         ModEntitySpawns.addSpawns();
-
-        LOGGER.info("Hello Fabric world!");
     }
 }
