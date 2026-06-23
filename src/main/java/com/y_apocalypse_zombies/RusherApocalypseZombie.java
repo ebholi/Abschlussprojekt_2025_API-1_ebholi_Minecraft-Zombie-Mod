671e6b5191a5879a9bb4bc8174f9426a569c2fc3
ebholi <oliver.ebhardt@lernende.bbw.ch>
2026-06-23 14:12:31 +0200

    Clean Code + Rush stop Bugfix on Reinforcements

--- src/main/java/com/y_apocalypse_zombies/RusherApocalypseZombie.java
+++ src/main/java/com/y_apocalypse_zombies/RusherApocalypseZombie.java
@@ -19,7 +19,6 @@ import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.monster.zombie.Zombie;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.Heightmap;
-import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animatable.manager.AnimatableManager;
@@ -32,6 +31,7 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoEntity {
     private int rushCooldown = 0; // 400 ticks / 20s of cooldown
 
+    // Entity Constructor
     public RusherApocalypseZombie(EntityType<? extends Zombie> entityType, Level world) {
         super(entityType, world);
 
@@ -39,8 +39,16 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
         this.spawnBaseReinforcements = false;
     }
 
-    private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache((GeoAnimatable) this);
+    // Cache required by GeckoLib
+    private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
 
+    // Required function for GeckoLib to function, gives GeckoLib the entity's Cache
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return geoCache;
+    }
+
+    // Creating the Rusher's Attributes
     public static AttributeSupplier.Builder createAttributes() {
         return BaseApocalypseZombie.createAttributes()
                 .add(Attributes.MAX_HEALTH, 14.0)
@@ -48,12 +56,14 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
                 .add(Attributes.ARMOR, 1);
     }
 
+    // Data Fields to be shared / synced between Client & Server
     private static final EntityDataAccessor<Boolean> IS_RUSHING =
             SynchedEntityData.defineId(RusherApocalypseZombie.class, EntityDataSerializers.BOOLEAN);
-
+    // Whether or not the Rusher hit his Target after the Rush
     private static final EntityDataAccessor<Boolean> HIT_RUSH =
             SynchedEntityData.defineId(RusherApocalypseZombie.class, EntityDataSerializers.BOOLEAN);
 
+    // Initial Values of shared fields
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
         super.defineSynchedData(builder);
@@ -61,34 +71,40 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
         builder.define(HIT_RUSH, false);
     }
 
+    // Functions to change the value of a shared field
     public void setRushing(boolean value) {
         this.entityData.set(IS_RUSHING, value);
-    }
-
-    public boolean isRushing() {
-        return this.entityData.get(IS_RUSHING);
     }
 
     public void setHitRush(boolean value) {
         this.entityData.set(HIT_RUSH, value);
     }
 
+    // Getting the value of a shared field
+    public boolean isRushing() {
+        return this.entityData.get(IS_RUSHING);
+    }
+
     public boolean isHitRush() {
         return this.entityData.get(HIT_RUSH);
     }
 
+    // This function triggers whenever a Rusher hits its target
     @Override
     public boolean doHurtTarget(ServerLevel serverLevel, Entity target) {
         boolean value = super.doHurtTarget(serverLevel, target);
+        // Plays attack animation
         if (value) {
             this.triggerAnim("attack", "attack");
         }
-        if (isRushing()) {
+        // Stops the Rush animation when the Rusher hits its target
+        if (isRushing() && value) {
             this.entityData.set(HIT_RUSH, true);
         }
         return value;
     }
 
+    // Animations
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
         controllers.add(new AnimationController<>("walk", 2, this::walkAnimController));
@@ -97,35 +113,34 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
                 .triggerableAnim("attack", RawAnimation.begin().thenPlay("attack")));
     }
 
+    // Normal walking
     protected PlayState walkAnimController(AnimationTest<RusherApocalypseZombie> rusher) {
         if (rusher.isMoving()) {
             return rusher.setAndContinue(RawAnimation.begin().thenLoop("walk_loop"));
         }
+        // Stops the animation
         rusher.controller().reset();
         return PlayState.STOP;
     }
 
+    // Rush animation
     protected PlayState rushAnimController(AnimationTest<RusherApocalypseZombie> rusher) {
         if (rusher.isMoving() && this.isRushing()) {
             return rusher.setAndContinue(RawAnimation.begin().thenLoop("rush_loop"));
         }
+        // Stops the animation
         rusher.controller().reset();
         return PlayState.STOP;
     }
 
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return geoCache;
-    }
-
-    // Adds custom Rush Goal
+    // Adds Rush Ability in form of a new Goal
     @Override
     protected void registerGoals() {
         super.registerGoals();
         this.goalSelector.addGoal(1, new RushGoal());
     }
 
-    // Actual Rush Goal Logic
+    // Rush Goal definition
     public class RushGoal extends Goal {
         private static final Identifier RUSH_SPEED = Identifier.fromNamespaceAndPath(YApocalypseZombies.MOD_ID, "rush_speed");
         private LivingEntity target;
@@ -138,10 +153,12 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
             return target != null && rushCooldown <= 0 && RusherApocalypseZombie.this.distanceTo(target) >= 8;
         }
 
+        // Starts the Ability
         @Override
         public void start() {
             RusherApocalypseZombie.this.setRushing(true);
             gotToTarget = false;
+            // Increases Speed
             RusherApocalypseZombie.this.getAttribute(Attributes.MOVEMENT_SPEED).addOrReplacePermanentModifier(
                     new AttributeModifier(
                             RUSH_SPEED,
@@ -151,6 +168,7 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
             );
         }
 
+        // Stops the Rush and resets the Rusher to normal
         @Override
         public void stop() {
             RusherApocalypseZombie.this.getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(RUSH_SPEED);
@@ -163,10 +181,11 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
             }
         }
 
-        // Control what happens every tick (0.05s)
+        // Control what happens every tick
         @Override
         public void tick() {
             target = RusherApocalypseZombie.this.getTarget();
+            // Navigating towards Target
             if (target != null) {
                 RusherApocalypseZombie.this.getNavigation().moveTo(target, 0.8);
             }
@@ -175,6 +194,7 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
         @Override
         public boolean canContinueToUse() {
             target = RusherApocalypseZombie.this.getTarget();
+            // Checks if the target was lost, if the Rush was successful or if Rushing is false
             if (target == null || isHitRush() || !isRushing()) {
                 gotToTarget = true;
                 setHitRush(false);
@@ -184,7 +204,7 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
         }
     }
 
-    // Runs for every Tick, even when Ability is inactive
+    // Runs for every Tick, decreases the Rush Cooldown
     @Override
     public void tick() {
         super.tick();
@@ -193,22 +213,30 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
         }
     }
 
-    // Reinforcement Spawning
+    // Functions that happen when the Rusher takes damage
     @Override
     public boolean hurtServer(ServerLevel serverLevel, DamageSource damageSource, float f) {
         Difficulty difficulty = serverLevel.getDifficulty();
         if (!super.hurtServer(serverLevel, damageSource, f)) {
+            // Cancels the hurtServer if the Rusher isn't taking damage
             return false;
-        } else if (!(damageSource.getEntity() instanceof LivingEntity) || this.isReinforcement()) {
+        } else if (!(damageSource.getEntity() instanceof LivingEntity)) {
+            // Simply returns the Damage if the Damage source isn't an Entity (like fall damage).
+            return true;
+        } else if (this.isReinforcement()) {
+            setRushing(false); // Cancels Rush after taking damage
             return true;
         } else {
             setRushing(false);
+            // 1 in 12 Chance to spawn a reinforcement
             if (serverLevel.random.nextInt(12) == 0) {
                 BaseApocalypseZombie reinforcement = ModEntityTypes.RUSHER_APOCALYPSE_ZOMBIE_ENTITY_TYPE
                         .create(serverLevel, EntitySpawnReason.REINFORCEMENT);
+                // Only spawns the reinforcement on Normal or Hard difficulty
                 if (reinforcement != null
                         && (difficulty == Difficulty.NORMAL || difficulty == Difficulty.HARD)) {
                     reinforcement.setIsReinforcement(true);
+                    // Nerfing reinforcements
                     if (difficulty == Difficulty.NORMAL) {
                         reinforcement.setHealth(reinforcement.getHealth() - 4);
                         reinforcement.setCanPickUpLoot(false);
@@ -226,7 +254,7 @@ public class RusherApocalypseZombie extends BaseApocalypseZombie implements GeoE
                                     (int) this.getZ() + serverLevel.random.nextInt(21) - serverLevel.random.nextInt(21)
                             )
                     );
-
+                    // Placing the reinforcement in the game
                     reinforcement.setPos(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
                     serverLevel.addFreshEntity(reinforcement);
                 }
